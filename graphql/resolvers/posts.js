e5ecacdacafd44eb9763b9457520f1741e6c9b62
jfar41 <68749301+jfar41@users.noreply.github.com>
2020-11-23 19:24:41 -0800

    Added comments, ability to delete comments, and like posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,13 @@
+const { AuthenticationError, UserInputError } = require('apollo-server');
+
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Query: {
         async getPosts(){
             try{
-                const posts = await Post.find();    //when no specifying .find() you get ALL, which we want rn
+                const posts = await Post.find().sort({createdAt: -1});    //when no specifying .find() you get ALL, which we want rn
                 return posts;
             } catch (err){
                 throw new Error(err);
@@ -25,7 +28,50 @@ module.exports = {
     },
     Mutation: {
         async createPost(_, { body }, context){     //bc of what did in index.js, this context now has request body so we can access header and determine user authentication
-            
+            const user = checkAuth(context);
+            console.log(user)
+            const newPost = new Post({
+                body,       //this is ok since we already destructured body above
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            return post;
+        },
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context);
+            try {
+                const post = await Post.findById(postId);
+                if (user.username === post.username) {
+                    await post.delete();
+                    return 'Post deleted succesfully';
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                }
+            } catch(err) {
+                throw new Error(err);
+            }
+        },
+        async likePost(_, { postId }, context) {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if(post) {
+                if (post.likes.find(like => like.username === username)) {
+                    //Post already liked, so unlike it
+                    post.likes = post.likes.filter(like => like.username !== username);
+                } else {
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found')
         }
     }
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,19 @@ module.exports = gql`
         body: String!
         createdAt: String!
         username: String!
+        comments: [Comment]!
+        likes: [Like]!
+    }
+    type Comment {
+        id: ID!
+        createdAt: String!
+        username: String!
+        body: String!
+    }
+    type Like {
+        id: ID!
+        createdAt: String!
+        username: String!
     }
     type User {
         id: ID!
@@ -20,7 +33,7 @@ module.exports = gql`
         confirmPassword: String!
         email: String!
     }
-    type Query{
+    type Query {
         getPosts: [Post]
         getPost(postId: ID!): Post
     }
@@ -29,5 +42,8 @@ module.exports = gql`
         login(username: String!, password: String!): User!
         createPost(body: String!): Post!
         deletePost(postId: ID!): String!
+        createComment(postId: String!, body: String!): Post!
+        deleteComment(postId: ID!, commentId: ID!): Post!
+        likePost(postId: ID!): Post!
     }
 `;
